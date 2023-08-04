37ca3d7af655cae4182059862a5f7f3fa680f739
CharlesMartinUK <charles_martin@hotmail.co.uk>
2023-08-04 17:06:00 +0100

    Got rid on unneeded main function

--- src/index.js
+++ src/index.js
@@ -5,31 +5,28 @@ var plateau_1 = require("./plateau");
 var executeInstructions_1 = require("./executeInstructions");
 var logic_1 = require("./logic");
 var sleep = require('system-sleep');
-function main() {
-    var platDim = (0, logic_1.input)('Input width and height of Plateau (eg 5 5): ', 2, function (list) {
-        return [(0, logic_1.positiveNumberWithLimit)(list[0], 10), (0, logic_1.positiveNumberWithLimit)(list[1], 10)];
+var platDim = (0, logic_1.input)('Input width and height of Plateau (eg 5 5): ', 2, function (list) {
+    return [(0, logic_1.positiveNumberWithLimit)(list[0], 10), (0, logic_1.positiveNumberWithLimit)(list[1], 10)];
+});
+var plat = new plateau_1.Plateau(Number(platDim[0]), Number(platDim[1]));
+var _loop_1 = function () {
+    console.log("*********** ROVER INPUT *************");
+    var firstLine = (0, logic_1.input)('Input x y and inital direction (eg 1 2 N): ', 3, function (list) {
+        return [(0, logic_1.positiveNumberWithLimit)(list[0], plat.width),
+            (0, logic_1.positiveNumberWithLimit)(list[1], plat.height),
+            (0, logic_1.directionsOnly)(list[2])
+        ];
     });
-    var plat = new plateau_1.Plateau(Number(platDim[0]), Number(platDim[1]));
-    var _loop_1 = function () {
-        console.log("*********** ROVER INPUT *************");
-        var firstLine = (0, logic_1.input)('Input x y and inital direction (eg 1 2 N): ', 3, function (list) {
-            return [(0, logic_1.positiveNumberWithLimit)(list[0], plat.width),
-                (0, logic_1.positiveNumberWithLimit)(list[1], plat.height),
-                (0, logic_1.directionsOnly)(list[2])
-            ];
-        });
-        var secondLine = (0, logic_1.input)('Movement directions (eg LMMRLLR): ', 1, function (list) {
-            return [(0, logic_1.movementsOnly)(list[0])];
-        });
-        var rover = new rover_1.Rover(Number(firstLine[0]), Number(firstLine[1]), String(firstLine[2]));
-        (0, executeInstructions_1.executeInstructions)(rover, plat, String(secondLine), function () {
-            (0, logic_1.printMap)(rover.x, rover.y, plat.width, plat.height);
-            console.log("CURRENT: Rover End Position and direction", rover.x, " ", rover.y, " ", rover.direction);
-            sleep(1000);
-        });
-    };
-    while (true) {
-        _loop_1();
-    }
+    var secondLine = (0, logic_1.input)('Movement directions (eg LMMRLLR): ', 1, function (list) {
+        return [(0, logic_1.movementsOnly)(list[0])];
+    });
+    var rover = new rover_1.Rover(Number(firstLine[0]), Number(firstLine[1]), String(firstLine[2]));
+    (0, executeInstructions_1.executeInstructions)(rover, plat, String(secondLine), function () {
+        (0, logic_1.printMap)(rover.x, rover.y, plat.width, plat.height);
+        console.log("CURRENT: Rover End Position and direction", rover.x, " ", rover.y, " ", rover.direction);
+        sleep(1000);
+    });
+};
+while (true) {
+    _loop_1();
 }
-main();
--- src/index.ts
+++ src/index.ts
@@ -5,44 +5,41 @@ import {input, movementsOnly, directionsOnly, positiveNumberWithLimit, printMap}
 
 var sleep = require('system-sleep');
 
-function main() {
+
 
 	
-	const platDim = input('Input width and height of Plateau (eg 5 5): ',2, (list) => {
-		return [positiveNumberWithLimit(list[0],10),  positiveNumberWithLimit(list[1],10) ]
+const platDim = input('Input width and height of Plateau (eg 5 5): ',2, (list) => {
+	return [positiveNumberWithLimit(list[0],10),  positiveNumberWithLimit(list[1],10) ]
+});
+	
+	
+const plat = new Plateau(Number(platDim[0]), Number(platDim[1]) )
+	
+while(true) { // loop for each rover
+	
+	console.log("*********** ROVER INPUT *************")
+	const firstLine = input('Input x y and inital direction (eg 1 2 N): ',3, (list) => {
+		return [positiveNumberWithLimit(list[0], plat.width),  
+				positiveNumberWithLimit(list[1], plat.height),
+				directionsOnly(list[2])	
+				]
 	});
-	
-	
-	const plat = new Plateau(Number(platDim[0]), Number(platDim[1]) )
-	
-	while(true) { // loop for each rover
-	
-		console.log("*********** ROVER INPUT *************")
-		const firstLine = input('Input x y and inital direction (eg 1 2 N): ',3, (list) => {
-			return [positiveNumberWithLimit(list[0], plat.width),  
-					positiveNumberWithLimit(list[1], plat.height),
-					directionsOnly(list[2])	
-					]
-		});
 		
 		
-		let secondLine = input('Movement directions (eg LMMRLLR): ', 1, (list) => {
-			return [ movementsOnly(list[0]) ]
-		});
+	let secondLine = input('Movement directions (eg LMMRLLR): ', 1, (list) => {
+		return [ movementsOnly(list[0]) ]
+	});
 		
 			
-		const rover = new Rover( Number(firstLine[0]), Number(firstLine[1]), String(firstLine[2]) )
+	const rover = new Rover( Number(firstLine[0]), Number(firstLine[1]), String(firstLine[2]) )
 		
 		
-		executeInstructions(rover, plat, String(secondLine), () => {
-			printMap(rover.x, rover.y, plat.width, plat.height)
-			console.log("CURRENT: Rover End Position and direction",rover.x," ", rover.y," ", rover.direction)
-			sleep(1000)
-		}			)
+	executeInstructions(rover, plat, String(secondLine), () => {
+		printMap(rover.x, rover.y, plat.width, plat.height)
+		console.log("CURRENT: Rover End Position and direction",rover.x," ", rover.y," ", rover.direction)
+		sleep(1000)
+	}			)
 		
 		//console.log("Rover End Position and direction",rover.x," ", rover.y," ", rover.direction)
-	}
-	
 }
-
-main()
+	
